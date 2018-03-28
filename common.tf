8e33442ee40732b405f569fabbfc624ecd4b8b7c
Yongseon Lee <yslee@devsisters.com>
2018-03-28 10:47:44 +0900

    Add tfvars.example, stop using remote state

--- common.tf
+++ common.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region  = "${var.region}"
   profile = "${var.profile}"
 }
-
-terraform {
-  backend "s3" {
-    bucket  = "king-terraform-state"
-    key     = "king-swan.tfstate"
-    region  = "ap-northeast-1"
-    encrypt = "true"
-  }
-}
