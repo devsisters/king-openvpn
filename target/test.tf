58a191cb7e4f057a4365d7ab2c9322760d0faf95
Yongseon Lee <yslee@devsisters.com>
2018-04-19 15:50:50 +0900

    Test instance for vpc peering

--- target/test.tf
+++ target/test.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-northeast-2"
+  region = "ap-southeast-1"
 }
 
 data "terraform_remote_state" "king_vpn" {
@@ -29,38 +29,61 @@ data "aws_ami" "ubuntu_xenial" {
   owners = ["099720109477"]
 }
 
-resource "aws_customer_gateway" "king_seoul" {
-  bgp_asn    = 65000
-  ip_address = "${data.terraform_remote_state.king_vpn.king_swan_public_ip}"
-  type       = "ipsec.1"
-
-  tags {
-    Name = "king-vpn"
-  }
-}
-
-module "custom_seoul_vpc" {
-  source = "git@github.com:devsisters/king-openvpn.git?ref=master//modules/vpc"
+module "custom_singapore_vpc" {
+  source = "git@github.com:devsisters/king-openvpn.git?ref=modules//modules/vpc"
 
   name       = "king-custom-vpc"
-  aws_region = "ap-northeast-2"
+  aws_region = "ap-southeast-1"
   az_main    = "a"
   az_sub     = "c"
   cidr_block = "172.30.0.0/16"
 }
 
-resource "aws_instance" "king_seoul" {
+module "vpc_peering" {
+  source = "git@github.com:devsisters/king-openvpn.git?ref=modules//modules/peering"
+
+  local_vpc_id                 = "${module.custom_singapore_vpc.vpc_id}"
+  local_region                 = "ap-southeast-1"
+  local_public_route_table_id  = "${module.custom_singapore_vpc.aws_public_route_table_id}"
+  local_private_route_table_id = "${module.custom_singapore_vpc.aws_private_route_table_id}"
+
+  remote_vpc_id                 = "${data.terraform_remote_state.king_vpn.king_vpc_id}"
+  remote_region                 = "ap-northeast-1"
+  remote_public_route_table_id  = "${data.terraform_remote_state.king_vpn.aws_public_route_table_id}"
+  remote_private_route_table_id = "${data.terraform_remote_state.king_vpn.aws_private_route_table_id}"
+}
+
+resource "aws_instance" "king_singapore" {
   ami           = "${data.aws_ami.ubuntu_xenial.id}"
   instance_type = "t2.micro"
-  key_name      = "${var.seoul_public_key_name}"
+  key_name      = "${var.singapore_public_key_name}"
 
-  vpc_security_group_ids = [
-    "${module.king_gateway.sg_id}",
-  ]
+  vpc_security_group_ids = ["${aws_security_group.king_singapore.id}"]
 
-  subnet_id = "${module.custom_seoul_vpc.public_common_subnet_id}"
+  subnet_id = "${module.custom_singapore_vpc.public_common_subnet_id}"
 
   tags {
-    Name = "king-seoul"
+    Name = "king-singapore"
+  }
+}
+
+resource "aws_security_group" "king_singapore" {
+  name   = "king-singapore"
+  vpc_id = "${module.custom_singapore_vpc.vpc_id}"
+
+  # SSH
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["${data.terraform_remote_state.king_vpn.king_vpn_private_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
